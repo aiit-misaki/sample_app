27a44feccc6231861334c9f56041d7bec3c3276f
aiit-misaki <117704465+aiit-misaki@users.noreply.github.com>
2023-05-20 02:34:38 +0000

    update authenticated?

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -20,7 +20,6 @@ class InvalidPasswordTest < UsersLogin
     post login_path, params: { session: { email:    @user.email,
                                 password: "invalid" } }
     assert_not is_logged_in?
-    assert_not is_logged_in?
     assert_response :unprocessable_entity
     assert_template 'sessions/new'
     assert_not flash.empty?
@@ -75,5 +74,10 @@ class LogoutTest < Logout
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "should still work after logout in second window" do
+    delete logout_path
+    assert_redirected_to root_url
   end
 end
