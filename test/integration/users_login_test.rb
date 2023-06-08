30f999093e40df4a41a8dadf9bd88a665f65de27
aiit-misaki <117704465+aiit-misaki@users.noreply.github.com>
2023-06-08 15:19:51 +0000

    Add account activation

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -15,12 +15,9 @@ class InvalidPasswordTest < UsersLogin
   end
 
   test "login with valid email/invalid password" do
-    get login_path
-    assert_template 'sessions/new'
     post login_path, params: { session: { email:    @user.email,
-                                password: "invalid" } }
+                                          password: "invalid" } }
     assert_not is_logged_in?
-    assert_response :unprocessable_entity
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
